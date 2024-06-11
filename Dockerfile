d852e7690d83c129c69c828e534c7beb5314b6f0
geraldhd <125950426+geraldhd@users.noreply.github.com>
2024-06-11 11:08:01 +0800

    backend

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use the official Node.js 14 image as the base image
+FROM node:14
+
+# Set the working directory inside the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json to the container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code to the container
+COPY . .
+
+# Expose the port on which your application runs
+EXPOSE 5000
+
+# Set environment variables
+ENV NODE_ENV=production
+
+# Command to run the application
+CMD ["npm", "start"]
---
+++ src/server.js
@@ -0,0 +1,57 @@
+const Hapi = require('@hapi/hapi');
+const jwt = require('@hapi/jwt');
+require('dotenv').config();
+
+const noAuthRoutes = require('./user/noAuthRoutes');
+const userRoutes = require('./user/routes');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const PORT = process.env.PORT || 5000; // Use the PORT environment variable or default to 5000
+
+const init = async () => {
+    const server = Hapi.server({
+        port: PORT,
+        host: '0.0.0.0', // Listen on all network interfaces
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register(jwt);
+
+    // Define the JWT authentication strategy
+    server.auth.strategy('jwt', 'jwt', {
+        keys: JWT_SECRET,
+        verify: {
+            aud: 'urn:audience:test',
+            iss: 'urn:issuer:test',
+            sub: false,
+            nbf: true,
+            exp: true,
+            maxAgeSec: 24 * 60 * 60,
+            timeSkewSec: 15
+        },
+        validate: (artifacts, request, h) => {
+            return {
+                isValid: true,
+                credentials: { user: artifacts.decoded.payload.sub }
+            };
+        }
+    });
+
+    // Default all routes to require authentication
+    server.auth.default('jwt');
+
+    // Register routes without authentication
+    server.route(noAuthRoutes);
+
+    // Register routes with authentication
+    server.route(userRoutes);
+
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
